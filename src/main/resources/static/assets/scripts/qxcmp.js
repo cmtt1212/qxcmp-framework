463ca06f79a2c8950f7f098a3e2f5de3fc940366
aaric <aaricchen@foxmail.com>
2017-08-21 15:45:26 +0800

    Fix materialize css tabs bugs, waiting for merge
    https://github.com/Dogfalo/materialize/commit/17046f4fecca8308bfd2ca542d81814ca139bae8

--- src/main/resources/static/assets/scripts/qxcmp.js
+++ src/main/resources/static/assets/scripts/qxcmp.js
@@ -5209,7 +5209,8 @@ if (jQuery) {
             $tabs_wrapper,
             $tab_width = Math.max($tabs_width, $this[0].scrollWidth) / $links.length,
             $indicator,
-            index = prev_index = 0,
+            index = 0,
+            prev_index = 0,
             clicked = false,
             clickedTimeout,
             transition = 300;
