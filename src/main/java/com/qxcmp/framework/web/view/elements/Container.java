eebfc988d7fc346b01c2bfb65f744dc70e6172ac
Aaric <aaricchen@foxmail.com>
2017-08-29 02:31:01 +0800

    Finish container element

--- src/main/java/com/qxcmp/framework/web/view/elements/Container.java
+++ src/main/java/com/qxcmp/framework/web/view/elements/Container.java
@@ -1,10 +1,12 @@
 package com.qxcmp.framework.web.view.elements;
 
 import com.qxcmp.framework.web.view.Component;
+import com.qxcmp.framework.web.view.support.TextAlignment;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Singular;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -13,11 +15,37 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = false)
 public class Container implements Component {
 
+    private boolean textContainer;
+
+    private boolean fluid;
+
+    @Builder.Default
+    private TextAlignment alignment = TextAlignment.NONE;
+
     @Singular
     private List<Component> components;
 
     @Override
     public String getFragmentFile() {
         return "qxcmp/elements/container";
+    }
+
+    @Override
+    public String getClassName() {
+        StringBuilder stringBuilder = new StringBuilder("ui container");
+
+        if (textContainer) {
+            stringBuilder.append(" text");
+        }
+
+        if (fluid) {
+            stringBuilder.append(" fluid");
+        }
+
+        if (StringUtils.isNotBlank(alignment.getClassName())) {
+            stringBuilder.append(" ").append(alignment.getClassName());
+        }
+
+        return stringBuilder.toString();
     }
 }
