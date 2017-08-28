afa1a558b7e35086ca58960e29142c45d299e066
aaric <aaricchen@foxmail.com>
2017-08-28 17:39:04 +0800

    Add login component

--- src/main/java/com/qxcmp/framework/web/controller/LoginPageController.java
+++ src/main/java/com/qxcmp/framework/web/controller/LoginPageController.java
@@ -3,9 +3,7 @@ package com.qxcmp.framework.web.controller;
 import com.qxcmp.framework.view.ModelAndViewBuilder;
 import com.qxcmp.framework.web.QXCMPController;
 import com.qxcmp.framework.web.view.Page;
-import com.qxcmp.framework.web.view.elements.BasicButton;
 import com.qxcmp.framework.web.view.elements.Container;
-import com.qxcmp.framework.web.view.elements.Segment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,10 +19,7 @@ public class LoginPageController extends QXCMPController {
     @GetMapping("/login")
     public ModelAndView loginPage() {
         return builder("qxcmp")
-                .addObject(Page.builder().component(Container.builder().component(BasicButton.builder().text("测试按钮").build())
-                        .component(Segment.builder().component(BasicButton.builder().text("分段按钮1").build()).build())
-                        .component(Segment.builder().component(BasicButton.builder().text("分段按钮2").build()).build())
-                        .build()).build())
+                .addObject(Page.builder().component(Container.builder().component(() -> "qxcmp/components/login").build()).build())
                 .build();
     }
 }
