d9fc343c53b00a561848b509cd4069f97ee2c77f
aaric <aaricchen@foxmail.com>
2017-09-04 13:52:54 +0800

    Change sample page layout

--- src/main/java/com/qxcmp/framework/web/controller/sample/SamplePageController.java
+++ src/main/java/com/qxcmp/framework/web/controller/sample/SamplePageController.java
@@ -1,11 +1,15 @@
 package com.qxcmp.framework.web.controller.sample;
 
 import com.qxcmp.framework.web.QXCMPController;
+import com.qxcmp.framework.web.view.containers.grid.AbstractGrid;
 import com.qxcmp.framework.web.view.containers.grid.CelledGrid;
 import com.qxcmp.framework.web.view.containers.grid.Col;
 import com.qxcmp.framework.web.view.containers.grid.Row;
 import com.qxcmp.framework.web.view.elements.container.Container;
 import com.qxcmp.framework.web.view.elements.html.Anchor;
+import com.qxcmp.framework.web.view.elements.html.H2;
+import com.qxcmp.framework.web.view.support.Alignment;
+import com.qxcmp.framework.web.view.support.Wide;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +21,19 @@ public class SamplePageController extends QXCMPController {
 
     @GetMapping("")
     public ModelAndView home() {
-        return page(() -> new Container().addComponent(new CelledGrid()
-                .addItem(new Row().addCol(new Col().addComponent(new Anchor("基本元素", "/test/sample/html"))))));
+        return page(() -> {
+
+            AbstractGrid grid = new CelledGrid();
+            grid.setAlignment(Alignment.CENTER);
+            grid
+                    .addItem(new Row().addCol(new Col(Wide.SIXTEEN).addComponent(new H2("无行为元素"))).addCol(new Col(Wide.SIXTEEN)
+                            .addComponent(new Anchor("基本元素", "/test/sample/html"))
+                    ))
+                    .addItem(new Row().addCol(new Col(Wide.SIXTEEN).addComponent(new H2("集合元素"))).addCol(new Col(Wide.SIXTEEN)))
+                    .addItem(new Row().addCol(new Col(Wide.SIXTEEN).addComponent(new H2("常用视图"))).addCol(new Col(Wide.SIXTEEN)))
+                    .addItem(new Row().addCol(new Col(Wide.SIXTEEN).addComponent(new H2("带行为组件"))).addCol(new Col(Wide.SIXTEEN)));
+
+            return new Container().addComponent(grid);
+        });
     }
 }
