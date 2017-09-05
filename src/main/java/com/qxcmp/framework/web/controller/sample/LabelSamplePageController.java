e4916ad321837b40be0fb00be0a8c56d0364c1d6
aaric <aaricchen@foxmail.com>
2017-09-05 16:07:43 +0800

    Add labels component

--- src/main/java/com/qxcmp/framework/web/controller/sample/LabelSamplePageController.java
+++ src/main/java/com/qxcmp/framework/web/controller/sample/LabelSamplePageController.java
@@ -7,10 +7,10 @@ import com.qxcmp.framework.web.view.containers.grid.Row;
 import com.qxcmp.framework.web.view.elements.container.Container;
 import com.qxcmp.framework.web.view.elements.header.ContentHeader;
 import com.qxcmp.framework.web.view.elements.icon.Icon;
-import com.qxcmp.framework.web.view.elements.label.ImageLabel;
-import com.qxcmp.framework.web.view.elements.label.Label;
+import com.qxcmp.framework.web.view.elements.label.*;
 import com.qxcmp.framework.web.view.elements.segment.Segment;
 import com.qxcmp.framework.web.view.support.ColumnCount;
+import com.qxcmp.framework.web.view.support.Pointing;
 import com.qxcmp.framework.web.view.support.Size;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +27,9 @@ public class LabelSamplePageController extends AbstractSamplePageController {
                 .addItem(new Row()
                         .addCol(new Col().addComponent(createLabelSegment()))
                         .addCol(new Col().addComponent(createImageLabelSegment()))
+                        .addCol(new Col().addComponent(createPointingLabelSegment()))
+                        .addCol(new Col().addComponent(createCircularSegment()))
+                        .addCol(new Col().addComponent(createLabelsSegment()))
                 )));
     }
 
@@ -46,4 +49,35 @@ public class LabelSamplePageController extends AbstractSamplePageController {
                 ;
     }
 
+    private Component createPointingLabelSegment() {
+        return new Segment().addComponent(new ContentHeader("指针标签", Size.LARGE))
+                .addComponent(new PointingLabel("标签文本").setColor(randomColor()).setUrl("#"))
+                .addComponent(new PointingLabel("标签文本", Pointing.BOTTOM_POINTING).setColor(randomColor()).setUrl("#"))
+                .addComponent(new PointingLabel("标签文本", Pointing.LEFT_POINTING).setColor(randomColor()).setUrl("#"))
+                .addComponent(new PointingLabel("标签文本", Pointing.RIGHT_POINTING).setColor(randomColor()).setUrl("#"))
+                ;
+    }
+
+    private Component createCircularSegment() {
+        return new Segment().addComponent(new ContentHeader("圆形标签", Size.LARGE))
+                .addComponent(new CircularLabel("1").setColor(randomColor()).setUrl("#"))
+                .addComponent(new CircularLabel("1").setColor(randomColor()).setUrl("#"))
+                .addComponent(new CircularLabel("1").setColor(randomColor()).setUrl("#"))
+                .addComponent(new CircularLabel("1").setColor(randomColor()).setUrl("#"))
+                .addComponent(new EmptyCircularLabel().setColor(randomColor()).setUrl("#"))
+                .addComponent(new EmptyCircularLabel().setColor(randomColor()).setUrl("#"))
+                .addComponent(new EmptyCircularLabel().setColor(randomColor()).setUrl("#"))
+                .addComponent(new EmptyCircularLabel().setColor(randomColor()).setUrl("#"))
+                ;
+    }
+
+    private Component createLabelsSegment() {
+        return new Segment().addComponent(new ContentHeader("标准标签组", Size.LARGE))
+                .addComponent(new Labels().setColor(randomColor())
+                        .addLabel(new Label("标签文本").setIcon(new Icon("user")))
+                        .addLabel(new Label("标签文本").setIcon(new Icon("user")))
+                        .addLabel(new Label("标签文本").setIcon(new Icon("user")))
+                        .addLabel(new Label("标签文本").setIcon(new Icon("user")))
+                );
+    }
 }
