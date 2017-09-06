3e9b4b48a31dd90e6668bfdec9b423d257501443
Aaric <aaricchen@foxmail.com>
2017-09-07 01:41:30 +0800

    Add input dropdown item search support

--- src/main/java/com/qxcmp/framework/web/view/modules/dropdown/item/InputItem.java
+++ src/main/java/com/qxcmp/framework/web/view/modules/dropdown/item/InputItem.java
@@ -1,19 +1,51 @@
 package com.qxcmp.framework.web.view.modules.dropdown.item;
 
-import com.qxcmp.framework.web.view.elements.input.AbstractInput;
 import lombok.Getter;
+import lombok.Setter;
 
+/**
+ * 搜索输入框
+ */
 @Getter
+@Setter
 public class InputItem extends AbstractDropdownItem implements DropdownItem {
 
-    private AbstractInput input;
+    /**
+     * 占位符
+     */
+    private String placeholder;
 
-    public InputItem(AbstractInput input) {
-        this.input = input;
+    /**
+     * 搜索图标是否在左边
+     */
+    private boolean leftIcon;
+
+    public InputItem(String placeholder) {
+        this.placeholder = placeholder;
     }
 
     @Override
     public String getFragmentName() {
         return "item-input";
+    }
+
+    @Override
+    public String getClassPrefix() {
+        return "ui";
+    }
+
+    @Override
+    public String getClassContent() {
+        return leftIcon ? "left" : "";
+    }
+
+    @Override
+    public String getClassSuffix() {
+        return "icon search input";
+    }
+
+    public InputItem setLeftIcon() {
+        setLeftIcon(true);
+        return this;
     }
 }
