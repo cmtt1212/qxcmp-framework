8ac230707b7de2bce041c7897ff91e929e9dd93b
aaric <aaricchen@foxmail.com>
2017-09-14 13:13:57 +0800

    Close #11 add form submit loading support

--- src/main/java/com/qxcmp/framework/web/controller/sample/modules/FormSamplePageController.java
+++ src/main/java/com/qxcmp/framework/web/controller/sample/modules/FormSamplePageController.java
@@ -1,15 +1,23 @@
 package com.qxcmp.framework.web.controller.sample.modules;
 
 import com.qxcmp.framework.web.controller.sample.AbstractSamplePageController;
+import com.qxcmp.framework.web.view.elements.button.Button;
 import com.qxcmp.framework.web.view.elements.grid.Col;
 import com.qxcmp.framework.web.view.elements.grid.VerticallyDividedGrid;
+import com.qxcmp.framework.web.view.elements.header.PageHeader;
 import com.qxcmp.framework.web.view.elements.message.InfoMessage;
 import com.qxcmp.framework.web.view.elements.segment.Segment;
 import com.qxcmp.framework.web.view.support.ColumnCount;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
+
+import static com.qxcmp.framework.web.view.elements.header.HeaderType.H4;
 
 @Controller
 @RequestMapping("/test/sample/form")
@@ -21,5 +29,16 @@ public class FormSamplePageController extends AbstractSamplePageController {
         return page().addComponent(new VerticallyDividedGrid().setTextContainer().setVerticallyPadded().setColumnCount(ColumnCount.ONE).addItem(new Col().addComponent(new Segment()
                 .addComponent(convertToForm(testSampleForm).setInfoMessage((InfoMessage) new InfoMessage("关于表单说明", "本表单展示了所有的表单组件").setCloseable()))))
         ).addObject(testSampleForm).build();
+    }
+
+    @PostMapping("")
+    public ModelAndView sample(@Valid TestSampleForm form) throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(3);
+
+        return page().addComponent(new VerticallyDividedGrid().setTextContainer().setVerticallyPadded().setColumnCount(ColumnCount.ONE).addItem(new Col().addComponent(new Segment()
+                .addComponent(new PageHeader(H4, "提交成功"))
+                .addComponent(new Button("返回", "/test/sample/form").setBasic().setPrimary())
+        ))).build();
     }
 }
--- src/main/java/com/qxcmp/framework/web/controller/sample/modules/TestSampleForm.java
+++ src/main/java/com/qxcmp/framework/web/controller/sample/modules/TestSampleForm.java
@@ -3,12 +3,12 @@ package com.qxcmp.framework.web.controller.sample.modules;
 import com.qxcmp.framework.web.view.annotation.form.Form;
 import com.qxcmp.framework.web.view.annotation.form.TextAreaField;
 import com.qxcmp.framework.web.view.annotation.form.TextInputField;
+import com.qxcmp.framework.web.view.modules.form.FormMethod;
 import lombok.Data;
 
-@Form
+@Form(action = "/test/sample/form", method = FormMethod.POST)
 @Data
 public class TestSampleForm {
-
 
     @TextInputField(value = "用户名", section = "基本资料", required = true, tooltip = "用户名/邮箱/手机", maxLength = 30)
     private String username;
