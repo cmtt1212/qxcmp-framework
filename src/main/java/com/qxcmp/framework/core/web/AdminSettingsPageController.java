754fac787ad67f436fd80f81e343b52b565a5bc3
Aaric <aaricchen@foxmail.com>
2017-09-22 23:23:22 +0800

    Revert "Revert "Add account right menu""
    This reverts commit 0865bef2b18a62cd648e77013aa02d3f98a9d01d.

--- src/main/java/com/qxcmp/framework/core/web/AdminSettingsPageController.java
+++ src/main/java/com/qxcmp/framework/core/web/AdminSettingsPageController.java
@@ -2,9 +2,12 @@ package com.qxcmp.framework.core.web;
 
 import com.google.common.collect.ImmutableList;
 import com.qxcmp.framework.account.AccountService;
-import com.qxcmp.framework.core.QXCMPSystemConfigConfiguration;
 import com.qxcmp.framework.web.QXCMPBackendController;
+import com.qxcmp.framework.web.view.elements.header.HeaderType;
+import com.qxcmp.framework.web.view.elements.header.PageHeader;
+import com.qxcmp.framework.web.view.elements.list.item.TextItem;
 import com.qxcmp.framework.web.view.elements.segment.Segment;
+import com.qxcmp.framework.web.view.support.Alignment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -29,7 +32,19 @@ public class AdminSettingsPageController extends QXCMPBackendController {
     private final AccountService accountService;
 
     @GetMapping("")
-    public ModelAndView sitePage(final AdminSettingsForm form) {
+    public ModelAndView settingsPage() {
+        return page().addComponent(new Segment().setAlignment(Alignment.CENTER)
+                .addComponent(new PageHeader(HeaderType.H1, "系统设置"))
+                .addComponent(new com.qxcmp.framework.web.view.elements.list.List().setSelection()
+                        .addItem(new TextItem("网站配置").setUrl(QXCMP_BACKEND_URL + "/settings/site"))
+                        .addItem(new TextItem("账户配置").setUrl(QXCMP_BACKEND_URL + "/settings/account"))
+                ))
+                .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置")
+                .build();
+    }
+
+    @GetMapping("/site")
+    public ModelAndView sitePage(final AdminSettingsSiteForm form) {
 
         form.setLogo(systemConfigService.getString(SYSTEM_CONFIG_SITE_LOGO).orElse(SYSTEM_CONFIG_SITE_LOGO_DEFAULT_VALUE));
         form.setFavicon(systemConfigService.getString(SYSTEM_CONFIG_SITE_FAVICON).orElse(SYSTEM_CONFIG_SITE_FAVICON_DEFAULT_VALUE));
@@ -43,31 +58,20 @@ public class AdminSettingsPageController extends QXCMPBackendController {
         form.setWatermarkPosition(WATERMARK_POSITIONS.get(systemConfigService.getInteger(SYSTEM_CONFIG_IMAGE_WATERMARK_POSITION).orElse(SYSTEM_CONFIG_IMAGE_WATERMARK_POSITION_DEFAULT_VALUE)));
         form.setWatermarkFontSize(systemConfigService.getInteger(SYSTEM_CONFIG_IMAGE_WATERMARK_FONT_SIZE).orElse(SYSTEM_CONFIG_IMAGE_WATERMARK_FONT_SIZE_DEFAULT_VALUE));
 
-        form.setAccountEnableUsername(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_USERNAME).orElse(false));
-        form.setAccountEnableEmail(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_EMAIL).orElse(false));
-        form.setAccountEnablePhone(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_PHONE).orElse(false));
-        form.setAccountEnableInvite(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_INVITE).orElse(false));
-
-        form.setThreadPoolSize(systemConfigService.getInteger(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_CORE_POOL_SIZE).orElse(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_CORE_POOL_SIZE_DEFAULT_VALUE));
-        form.setMaxPoolSize(systemConfigService.getInteger(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_MAX_POOL_SIZE).orElse(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_MAX_POOL_SIZE_DEFAULT_VALUE));
-        form.setQueueSize(systemConfigService.getInteger(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_QUEUE_CAPACITY).orElse(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_QUEUE_CAPACITY_DEFAULT_VALUE));
-
-        form.setSessionTimeout(systemConfigService.getInteger(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_TIMEOUT).orElse(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_TIMEOUT_DEFAULT_VALUE));
-        form.setMaxSessionCount(systemConfigService.getInteger(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_MAX_ACTIVE_COUNT).orElse(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_MAX_ACTIVE_COUNT_DEFAULT_VALUE));
-        form.setPreventLogin(systemConfigService.getBoolean(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_MAX_PREVENT_LOGIN).orElse(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_MAX_PREVENT_LOGIN_DEFAULT_VALUE));
-
         return page().addComponent(new Segment().addComponent(convertToForm(form)))
-                .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置")
+                .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置", QXCMP_BACKEND_URL + "/settings", "网站配置")
+                .setVerticalMenu(getVerticalMenu("网站配置"))
                 .addObject("selection_items_position", WATERMARK_POSITIONS)
                 .build();
     }
 
-    @PostMapping("")
-    public ModelAndView sitePage(@Valid final AdminSettingsForm form, BindingResult bindingResult) {
+    @PostMapping("/site")
+    public ModelAndView sitePage(@Valid final AdminSettingsSiteForm form, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             return page().addComponent(new Segment().addComponent(convertToForm(form).setErrorMessage(convertToErrorMessage(bindingResult, form))))
-                    .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置")
+                    .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置", QXCMP_BACKEND_URL + "/settings", "网站配置")
+                    .setVerticalMenu(getVerticalMenu("网站配置"))
                     .addObject("selection_items_position", WATERMARK_POSITIONS)
                     .build();
         }
@@ -84,22 +88,43 @@ public class AdminSettingsPageController extends QXCMPBackendController {
             systemConfigService.update(SYSTEM_CONFIG_IMAGE_WATERMARK_NAME, form.getWatermarkName());
             systemConfigService.update(SYSTEM_CONFIG_IMAGE_WATERMARK_POSITION, String.valueOf(WATERMARK_POSITIONS.indexOf(form.getWatermarkPosition())));
             systemConfigService.update(SYSTEM_CONFIG_IMAGE_WATERMARK_FONT_SIZE, String.valueOf(form.getWatermarkFontSize()));
+        });
+    }
 
-            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_USERNAME, String.valueOf(form.isAccountEnableUsername()));
-            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_EMAIL, String.valueOf(form.isAccountEnableEmail()));
-            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_PHONE, String.valueOf(form.isAccountEnablePhone()));
-            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_INVITE, String.valueOf(form.isAccountEnableInvite()));
+    @GetMapping("/account")
+    public ModelAndView accountPage(final AdminSettingsAccountForm form) {
 
-            systemConfigService.update(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_CORE_POOL_SIZE, String.valueOf(form.getThreadPoolSize()));
-            systemConfigService.update(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_MAX_POOL_SIZE, String.valueOf(form.getMaxPoolSize()));
-            systemConfigService.update(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_TASK_EXECUTOR_QUEUE_CAPACITY, String.valueOf(form.getQueueSize()));
+        form.setEnableUsername(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_USERNAME).orElse(false));
+        form.setEnableEmail(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_EMAIL).orElse(false));
+        form.setEnablePhone(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_PHONE).orElse(false));
+        form.setEnableInvite(systemConfigService.getBoolean(SYSTEM_CONFIG_ACCOUNT_ENABLE_INVITE).orElse(false));
 
-            systemConfigService.update(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_TIMEOUT, String.valueOf(form.getSessionTimeout()));
-            systemConfigService.update(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_MAX_ACTIVE_COUNT, String.valueOf(form.getMaxSessionCount()));
-            systemConfigService.update(QXCMPSystemConfigConfiguration.SYSTEM_CONFIG_SESSION_MAX_PREVENT_LOGIN, String.valueOf(form.isPreventLogin()));
+        return page().addComponent(new Segment().addComponent(convertToForm(form)))
+                .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置", QXCMP_BACKEND_URL + "/settings", "账户配置")
+                .setVerticalMenu(getVerticalMenu("账户配置"))
+                .build();
+    }
 
+    @PostMapping("/account")
+    public ModelAndView sitePage(@Valid final AdminSettingsAccountForm form, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return page().addComponent(new Segment().addComponent(convertToForm(form).setErrorMessage(convertToErrorMessage(bindingResult, form))))
+                    .setBreadcrumb("控制台", QXCMP_BACKEND_URL, "系统设置", QXCMP_BACKEND_URL + "/settings", "账户配置")
+                    .setVerticalMenu(getVerticalMenu("账户配置"))
+                    .build();
+        }
+
+        return submitForm(form, context -> {
+            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_USERNAME, String.valueOf(form.isEnableUsername()));
+            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_EMAIL, String.valueOf(form.isEnableEmail()));
+            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_PHONE, String.valueOf(form.isEnablePhone()));
+            systemConfigService.update(SYSTEM_CONFIG_ACCOUNT_ENABLE_INVITE, String.valueOf(form.isEnableInvite()));
             accountService.loadConfig();
         });
     }
 
+    private List<String> getVerticalMenu(String activeItem) {
+        return ImmutableList.of(activeItem, "网站配置", QXCMP_BACKEND_URL + "/settings/site", "账户配置", QXCMP_BACKEND_URL + "/settings/account");
+    }
 }
