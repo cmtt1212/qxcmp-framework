7b7ed753a4f81a9eb53a4fc53bdec9fc09ce8700
aaric <aaricchen@foxmail.com>
2017-09-30 15:17:19 +0800

    Add mall privilege

--- src/main/java/com/qxcmp/framework/core/QXCMPNavigationConfiguration.java
+++ src/main/java/com/qxcmp/framework/core/QXCMPNavigationConfiguration.java
@@ -23,6 +23,7 @@ public class QXCMPNavigationConfiguration implements NavigationConfigurator {
     public static final String NAVIGATION_ADMIN_SIDEBAR = "ADMIN-SIDEBAR";
     public static final String NAVIGATION_ADMIN_SIDEBAR_USER = NAVIGATION_ADMIN_SIDEBAR + "-USER";
     public static final String NAVIGATION_ADMIN_SIDEBAR_NEWS = NAVIGATION_ADMIN_SIDEBAR + "-NEWS";
+    public static final String NAVIGATION_ADMIN_SIDEBAR_MALL = NAVIGATION_ADMIN_SIDEBAR + "-MALL";
     public static final String NAVIGATION_ADMIN_SIDEBAR_MESSAGE = NAVIGATION_ADMIN_SIDEBAR + "-MESSAGE";
     public static final String NAVIGATION_ADMIN_SIDEBAR_WEIXIN = NAVIGATION_ADMIN_SIDEBAR + "-WEIXIN";
     public static final String NAVIGATION_ADMIN_SIDEBAR_FINANCE = NAVIGATION_ADMIN_SIDEBAR + "-FINANCE";
@@ -68,6 +69,7 @@ public class QXCMPNavigationConfiguration implements NavigationConfigurator {
         navigationService.add(new Navigation(NAVIGATION_ADMIN_SIDEBAR, "侧边导航栏")
                 .addItem(new Navigation(NAVIGATION_ADMIN_SIDEBAR_USER, "用户管理", QXCMP_BACKEND_URL + "/user").setOrder(10).setPrivilegesAnd(ImmutableSet.of(PRIVILEGE_USER)))
                 .addItem(new Navigation(NAVIGATION_ADMIN_SIDEBAR_NEWS, "新闻管理", QXCMP_BACKEND_URL + "/news").setOrder(20).setPrivilegesAnd(ImmutableSet.of(PRIVILEGE_NEWS)))
+                .addItem(new Navigation(NAVIGATION_ADMIN_SIDEBAR_MALL, "商城管理", QXCMP_BACKEND_URL + "/mall").setOrder(30).setPrivilegesAnd(ImmutableSet.of()))
                 .addItem(new Navigation(NAVIGATION_ADMIN_SIDEBAR_MESSAGE, "消息服务", QXCMP_BACKEND_URL + "/message").setOrder(40).setPrivilegesAnd(ImmutableSet.of(PRIVILEGE_MESSAGE)))
                 .addItem(new Navigation(NAVIGATION_ADMIN_SIDEBAR_WEIXIN, "微信公众平台", QXCMP_BACKEND_URL + "/weixin").setOrder(50).setPrivilegesAnd(ImmutableSet.of(PRIVILEGE_WEIXIN)))
                 .addItem(new Navigation(NAVIGATION_ADMIN_SIDEBAR_FINANCE, "财务管理", QXCMP_BACKEND_URL + "/finance").setOrder(60).setPrivilegesAnd(ImmutableSet.of(PRIVILEGE_FINANCE)))
--- src/main/java/com/qxcmp/framework/core/QXCMPSecurityConfiguration.java
+++ src/main/java/com/qxcmp/framework/core/QXCMPSecurityConfiguration.java
@@ -14,14 +14,14 @@ import org.springframework.context.annotation.Configuration;
 public class QXCMPSecurityConfiguration {
 
     /*
-    * 平台内置角色
-    * */
+     * 平台内置角色
+     * */
     public static final String ROLE_NEWS = "新闻编辑";
     public static final String ROLE_NEWS_DESCRIPTION = "内置角色：可以访问新闻管理、我的文章、我的栏目页面";
 
     /*
-    * 基本权限
-    * */
+     * 基本权限
+     * */
     public static final String PRIVILEGE_SYSTEM_ADMIN = "系统管理员权限";
     public static final String PRIVILEGE_SYSTEM_ADMIN_DESCRIPTION = "可以进入后台系统";
     public static final String PRIVILEGE_ADMIN_SETTINGS = "系统配置管理权限";
@@ -30,8 +30,8 @@ public class QXCMPSecurityConfiguration {
     public static final String PRIVILEGE_ADMIN_SECURITY_DESCRIPTION = "可以修改系统安全配置";
 
     /*
-    * 系统工具权限
-    * */
+     * 系统工具权限
+     * */
     public static final String PRIVILEGE_ADMIN_TOOL = "系统工具使用权限";
     public static final String PRIVILEGE_ADMIN_TOOL_DESCRIPTION = "可以使用系统工具，还需要具有具体工具的使用权限";
     public static final String PRIVILEGE_ADMIN_LOG = "平台日志管理权限";
@@ -44,28 +44,28 @@ public class QXCMPSecurityConfiguration {
     public static final String PRIVILEGE_ADMIN_SPIDER_DESCRIPTION = "可以管理平台蜘蛛";
 
     /*
-    * 用户管理模块
-    * */
+     * 用户管理模块
+     * */
     public static final String PRIVILEGE_USER = "用户管理权限";
-    public static final String PRIVILEGE_USER_DESCRIPTION = "可以查看用户资料";
+    public static final String PRIVILEGE_USER_DESCRIPTION = "可以查看用户管理入口页面";
     public static final String PRIVILEGE_USER_ROLE = "用户管理权限";
     public static final String PRIVILEGE_USER_ROLE_DESCRIPTION = "可以修改用户角色";
     public static final String PRIVILEGE_USER_STATUS = "用户管理权限";
     public static final String PRIVILEGE_USER_STATUS_DESCRIPTION = "可以修改用户状态";
 
     /*
-    * 财务模块权限
-    * */
+     * 财务模块权限
+     * */
     public static final String PRIVILEGE_FINANCE = "财务管理权限";
-    public static final String PRIVILEGE_FINANCE_DESCRIPTION = "可以查看财务模块信息";
+    public static final String PRIVILEGE_FINANCE_DESCRIPTION = "可以查看财务管理入口页面";
     public static final String PRIVILEGE_FINANCE_WEIXIN = "微信支付配置管理权限";
     public static final String PRIVILEGE_FINANCE_WEIXIN_DESCRIPTION = "可以修改微信支付配置";
 
     /*
-    * 消息服务权限
-    * */
+     * 消息服务权限
+     * */
     public static final String PRIVILEGE_MESSAGE = "消息服务使用权限";
-    public static final String PRIVILEGE_MESSAGE_DESCRIPTION = "可以查看平台消息服务配置";
+    public static final String PRIVILEGE_MESSAGE_DESCRIPTION = "可以查看消息服务入口页面";
     public static final String PRIVILEGE_MESSAGE_EMAIL_CONFIG = "邮件服务配置权限";
     public static final String PRIVILEGE_MESSAGE_EMAIL_CONFIG_DESCRIPTION = "可以修改邮件服务配置";
     public static final String PRIVILEGE_MESSAGE_EMAIL_SEND = "邮件发送权限";
@@ -76,10 +76,10 @@ public class QXCMPSecurityConfiguration {
     public static final String PRIVILEGE_MESSAGE_SMS_SEND_DESCRIPTION = "可以进行短信发送";
 
     /*
-    * 新闻模块权限
-    * */
+     * 新闻模块权限
+     * */
     public static final String PRIVILEGE_NEWS = "新闻管理权限";
-    public static final String PRIVILEGE_NEWS_DESCRIPTION = "可以查看新闻模块信息";
+    public static final String PRIVILEGE_NEWS_DESCRIPTION = "可以查看新闻管理入口页面";
     public static final String PRIVILEGE_NEWS_CHANNEL = "栏目管理权限";
     public static final String PRIVILEGE_NEWS_CHANNEL_DESCRIPTION = "可以对栏目进行管理";
     public static final String PRIVILEGE_NEWS_ARTICLE_AUDIT = "文章审核权限";
@@ -88,14 +88,26 @@ public class QXCMPSecurityConfiguration {
     public static final String PRIVILEGE_NEWS_ARTICLE_MANAGEMENT_DESCRIPTION = "可以对平台已发布文章进行管理，禁用或者删除";
 
     /*
-    * 微信公众号模块权限
-    * */
+     * 微信公众号模块权限
+     * */
     public static final String PRIVILEGE_WEIXIN = "微信公众号管理权限";
-    public static final String PRIVILEGE_WEIXIN_DESCRIPTION = "可以查看微信公众号模块信息";
+    public static final String PRIVILEGE_WEIXIN_DESCRIPTION = "可以查看微信公众号管理入口页面";
     public static final String PRIVILEGE_WEIXIN_CONFIG = "微信公众号配置管理权限";
     public static final String PRIVILEGE_WEIXIN_CONFIG_DESCRIPTION = "可以修改微信公众号配置";
     public static final String PRIVILEGE_WEIXIN_MENU = "微信公众号菜单管理权限";
     public static final String PRIVILEGE_WEIXIN_MENU_DESCRIPTION = "可以修改微信公众号菜单";
     public static final String PRIVILEGE_WEIXIN_MATERIAL = "微信公众号素材管理权限";
     public static final String PRIVILEGE_WEIXIN_MATERIAL_DESCRIPTION = "可以管理微信公众号素材";
+
+    /*
+     * 商城模块权限
+     * */
+    public static final String PRIVILEGE_MALL = "商城管理权限";
+    public static final String PRIVILEGE_MALL_DESCRIPTION = "可以查看商城管理入口页面";
+    public static final String PRIVILEGE_MALL_ORDER = "商城订单管理权限";
+    public static final String PRIVILEGE_MALL_ORDER_DESCRIPTION = "可以管理商城所有订单";
+    public static final String PRIVILEGE_MALL_COMMODITY = "商城商品管理权限";
+    public static final String PRIVILEGE_MALL_COMMODITY_DESCRIPTION = "可以管理商城所有商品";
+    public static final String PRIVILEGE_MALL_STORE = "商城店铺管理权限";
+    public static final String PRIVILEGE_MALL_STORE_DESCRIPTION = "可以管理商城店铺，店铺所有者和管理员能够管理自己店铺的商品和订单";
 }
