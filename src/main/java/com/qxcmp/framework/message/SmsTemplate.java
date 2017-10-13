951ebc5ed9c8c7337dcc6c92b4c851b34ecd0e64
aaric <aaricchen@foxmail.com>
2017-10-13 10:59:27 +0800

    Add custom sms template entity

---
+++ src/main/java/com/qxcmp/framework/message/SmsTemplate.java
@@ -0,0 +1,32 @@
+package com.qxcmp.framework.message;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 短信模板实体
+ */
+@Entity
+@Table
+@Data
+public class SmsTemplate {
+
+    /**
+     * 主键
+     */
+    @Id
+    private String id;
+
+    /**
+     * 短信业务名称
+     */
+    private String title;
+
+    /**
+     * 业务对应短信模板代码
+     */
+    private String template;
+}
