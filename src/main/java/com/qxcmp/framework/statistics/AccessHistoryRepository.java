0b1328bd2b66568d72719c0008fe0c992f5a1e67
aaric <aaricchen@foxmail.com>
2017-11-13 16:15:52 +0800

    Fix access history query issue

--- src/main/java/com/qxcmp/framework/statistics/AccessHistoryRepository.java
+++ src/main/java/com/qxcmp/framework/statistics/AccessHistoryRepository.java
@@ -13,6 +13,6 @@ import java.util.Date;
 @Repository
 interface AccessHistoryRepository extends JpaRepository<AccessHistory, Long>, JpaSpecificationExecutor<AccessHistory> {
 
-    @Query("select a.url as url, count(a.url) as nbr from AccessHistory a where a.dateCreated > :date group by a.url ")
+    @Query("select new com.qxcmp.framework.statistics.AccessHistoryPageResult(a.url, count(a)) from AccessHistory a where a.dateCreated > :date group by a.url")
     Page<AccessHistoryPageResult> findByDateCreatedAfter(@Param("date") Date date, Pageable pageable);
 }
--- src/main/java/com/qxcmp/framework/statistics/AdminStatisticPageController.java
+++ src/main/java/com/qxcmp/framework/statistics/AdminStatisticPageController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.qxcmp.framework.core.QXCMPConfiguration.QXCMP_BACKEND_URL;
 import static com.qxcmp.framework.core.QXCMPNavigationConfiguration.NAVIGATION_ADMIN_STATISTIC;
@@ -33,10 +35,10 @@ public class AdminStatisticPageController extends QXCMPController {
 
     @GetMapping("/pages")
     public ModelAndView statisticPagesPage() {
-        List<AccessHistoryPageResult> accessHistoryPageResults = accessHistoryService.findByDateCreatedAfter(DateTime.now().minusDays(1).toDate(), new PageRequest(0, 20)).getContent();
+        List<AccessHistoryPageResult> results = accessHistoryService.findByDateCreatedAfter(DateTime.now().minusDays(1).toDate(), new PageRequest(0, 20)).getContent().stream().sorted(Comparator.comparing(AccessHistoryPageResult::getNbr).reversed()).collect(Collectors.toList());
 
         return page()
-                .addComponent(convertToTable(stringObjectMap -> accessHistoryPageResults.forEach(accessHistoryPageResult -> stringObjectMap.put(accessHistoryPageResult.getUrl(), accessHistoryPageResult.getNbr()))))
+                .addComponent(convertToTable(stringObjectMap -> results.forEach(accessHistoryPageResult -> stringObjectMap.put(accessHistoryPageResult.getUrl(), accessHistoryPageResult.getNbr()))))
                 .setVerticalNavigation(NAVIGATION_ADMIN_STATISTIC, NAVIGATION_ADMIN_STATISTIC_PAGES)
                 .setBreadcrumb("控制台", "", "网站统计", "statistic", "页面统计")
                 .build();
