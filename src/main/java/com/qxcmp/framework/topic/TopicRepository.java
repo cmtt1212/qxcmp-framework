b1088c522ab0b019eefe5f0da3a9b77ea222b60a
aaric <aaricchen@foxmail.com>
2017-12-04 15:30:49 +0800

    Add topic service

---
+++ src/main/java/com/qxcmp/framework/topic/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.qxcmp.framework.topic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface TopicRepository extends JpaRepository<Topic, String>, JpaSpecificationExecutor<Topic> {
+}
